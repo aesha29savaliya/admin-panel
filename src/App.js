87bfb0a1a1bebedf3d29b3c4f1a5bfd1264a8509
aesha29savaliya <aeshasavaliya29@gmail.com>
2023-12-15 14:37:31 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Dashboard from './Components/Dashboard';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
+function App() {
+          return (
+                    <div className="App">
+                    
+
+                    <Header />
+                    <Sidebar />
+                    <Dashboard />
+                    <Footer />
+                    
+                    </div>
+          );
+}
+
+export default App;
